b52809499f9f35c89a95222b19a62168a6352486
Olivier Mignot <olipt.mignot@gmail.com>
2023-11-08 17:27:41 +0100

    added getPlanets and getClimates fetch method

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,10 @@
 import { ofetch } from 'ofetch';
+const api = ofetch.create({baseURL: 'http://127.0.0.1:8000/api'}); //serveur local de Laravel
+
+export const getPlanets = async () => {
+  return api(`/planets`, { method: 'GET' });
+}
+
+export const getClimates = async () => {
+  return api(`/climates`, { method: 'GET' });
+}
--- src/views/PlanetsListView.vue
+++ src/views/PlanetsListView.vue
@@ -1,11 +1,44 @@
 <script setup>
   import H1TitleLayout from '@/components/layouts/H1TitleLayout.vue';
+  import { getClimates, getPlanets } from '@/services/api.js';
+  import { ref, onBeforeMount } from 'vue';
+  import TailSpin from '@/components/icons/TailSpin.vue';
+
+  const planets = ref(undefined);
+  const isViewLoaded = ref(false);
+
+  onBeforeMount(() => {
+    setTimeout(() => {
+      getPlanets()
+      .then(response => planets.value = response)
+      .catch(error => console.error(error));
+      isViewLoaded.value = true;
+    }, "1500");
+  });
 </script>
 
 <template>
   <H1TitleLayout>Nos destinations</H1TitleLayout>
+
+  <div class="container">
+    <aside class="filters-container">
+
+    </aside>
+
+    <main class="list-container">
+      <p v-for="planet in planets" :key="planet.id">{{ planet.name }}</p>
+      <TailSpin v-show="!isViewLoaded" class="loader"/>
+    </main>
+  </div>
 </template>
 
 <style lang='scss' scoped>
-  
+  .container {
+    max-width: $xl-breakpoint;
+    margin: 3rem auto;
+  }
+
+  .filters-container, .list-container {
+    background: $color-dark-blue2;
+  }
 </style>
