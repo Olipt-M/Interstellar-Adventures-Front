dc965b94110f8eed8bb5effb26bb3eac2da11522
Paolo Dos Santos <paolo.a.dsts@gmail.com>
2023-11-10 12:29:52 +0100

    WIP on Carousel component for the HomeView

---
+++ src/components/Carousel.vue
@@ -0,0 +1,48 @@
+<script>
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+import { getPlanets } from '@/services/api.js';
+import { ref, onBeforeMount } from 'vue';
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  setup() {
+    const planets = ref([]);
+
+    onBeforeMount(async () => {
+      try {
+        const response = await getPlanets();
+        planets.value = response.data;
+        console.log(planets.value);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données des planètes :', error);
+      }
+    });
+
+    return { planets };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <carousel :items-to-show="1">
+      <slide v-for="planet in planets" :key="planet.id">
+        <img :src="planet.picture" :alt="planet.name">
+      </slide>
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </div>
+</template>
+
+<style scoped>
+/* Styles spécifiques au composant si nécessaire */
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
   import H1TitleLayout from '@/components/layouts/H1TitleLayout.vue';
+  import Carousel from '@/components/Carousel.vue';
+
 </script>
 
 <template>
-  <H1TitleLayout>Interstellar Adventures, c bo</H1TitleLayout>
+  <div>
+    <H1TitleLayout>Interstellar Adventures</H1TitleLayout>
+    <Carousel />
+  </div>
 </template>
 
 <style lang='scss' scoped>
