7e4e4efbeb51f280f9c67e5ba491c9fb04d6e0ea
Paolo Dos Santos <paolo.a.dsts@gmail.com>
2023-11-13 15:58:37 +0100

    autoplay carousel added but no images for the moment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,36 @@
 <script setup>
   import H1TitleLayout from '@/components/layouts/H1TitleLayout.vue';
+  import { getPlanets } from '@/services/api.js';
+  import { ref, onBeforeMount } from 'vue';
   import Carousel from '@/components/Carousel.vue';
+  // import PlanetImageCard from '@/components/cards/PlanetImageCard.vue';
+
+  const planets = ref(undefined);
+
+  onBeforeMount(() => {
+    getPlanets()
+       .then(response => planets.value = response)
+       .catch(error => console.error(error));
+  });
 
 </script>
 
 <template>
-  <div>
     <H1TitleLayout>Interstellar Adventures</H1TitleLayout>
-    <Carousel/>
-  </div>
+    <p>Explore the vast expanse of our solar system</p>
+    <div>
+      <!-- <PlanetImageCard v-for="planet in planets" :key="planet.id" :planet="planet"/> -->
+      <Carousel/>
+    </div>
 </template>
 
 <style lang='scss' scoped>
+
+  p {
+    text-align: center;
+    color: $color-light;
+    padding: 3rem;
+    margin: 0;
+  }
   
 </style>
