3dee9439072d1e97c4e89f3d06fd2aab522e7125
Olivier Mignot <olipt.mignot@gmail.com>
2023-11-14 10:40:07 +0100

    creation of the MainButton

---
+++ src/components/buttons/MainButton.vue
@@ -0,0 +1,44 @@
+<script setup>
+  const props = defineProps({
+    colorMode: {
+      type: String,
+      required: true
+    }
+  });
+</script>
+
+<template>
+  <button v-if="colorMode==='light'" type="button" class="light-mode"><slot/></button>
+  <button v-else="colorMode==='dark'" type="button" class="dark-mode"><slot/></button>
+</template>
+
+<style lang="scss" scoped>
+  button {
+    cursor: pointer;
+    padding: 0.5rem 4rem;
+    border-radius: 1rem;
+    font-size: 2rem;
+
+    &.light-mode {
+      border: 1px solid $color-light;
+      background: $color-light;
+      color: $color-night-blue;
+
+      &:hover {
+        background: $color-dark-blue1;
+        color: $color-light;
+      }
+    }
+
+    &.dark-mode {
+      border: 1px solid $color-light;
+      background: $color-dark-blue1;
+      color: $color-light;
+
+      &:hover {
+        background: $color-light;
+        color: $color-night-blue;
+      }
+    }
+  }
+</style>
