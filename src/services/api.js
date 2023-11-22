0bd589e60085187057038573b4601620e1ca13c3
Olivier <olipt.mignot@gmail.com>
2023-11-22 22:05:38 +0100

    removed useless lines and parameters in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,9 @@
 import { ofetch } from 'ofetch';
 const api = ofetch.create({baseURL: 'http://127.0.0.1:8000/api'}); //serveur local de Laravel
 
-export const getPlanets = async (page = 1, limit = 5) => {
+export const getPlanets = async (page, limit) => {
   return api(`/planets/${page}/${limit}`, { method: 'GET' });
 }
-
-// export const getNbOfPlanets = async () => api(`/planets`, { method: 'GET' })
-//   .then(response => response.length)
 
 export const getPlanet = async (id) => {
   return api (`/planets/${id}`, { method: 'GET' });
